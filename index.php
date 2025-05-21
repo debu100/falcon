b373c093adf7f6243c1a9562ceb03763351c0954
debu100 <debajitchakraborty96@gmail.com>
2025-05-21 10:38:31 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+Main Template File
+
+@package Falcon
+
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Falcon WordPress Theme</title>
+</head>
+<body>
+    <h1>hi</h1>
+</body>
+</html>
